7a7b404cffcaa923b9499253404e30202593052d
garaemon <garaemon@gmail.com>
2010-11-15 14:29:59 +0900

    fix typos

--- nurarihyon.lisp
+++ nurarihyon.lisp
@@ -46,7 +46,7 @@
    #:make-matrix33 #:make-identity-matrix3
    #:make-matrix44 #:make-identity-matrix4
    #:make-identity-matrix
-   #:matrix-dimensions #:matrix-row-dimensions #:matrix-column-dimensions
+   #:matrix-dimensions #:matrix-row-dimension #:matrix-column-dimension
    #:copy-matrix #:copy-matrix*
    #:m+ #:m- #:m* #:mv*
    #:transpose
