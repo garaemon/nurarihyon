a15288367d446bd2dded7a93133da42408206d17
garaemon <garaemon@gmail.com>
2010-11-20 19:49:48 +0900

    use qw to access w element of quaternion in identity-quaternion

--- quaternion.lisp
+++ quaternion.lisp
@@ -18,7 +18,7 @@ it means [1; #(0 0 0)]"
   (let ((ret ($make-vector4)))
     (declare (type (simple-array double-float (4)) ret))
     ;; in nurarihyon, we use angle-axis quaternion, not axis-angle
-    (setf [ret 0] 1.0d0)
+    (setf (qw ret) 1.0d0)
     (the (simple-array double-float (4)) ret)))
 
 (defmacro qx (q)
