731eb169ca40fd92a6e05181734c1b0aa9659874
garaemon <garaemon@gmail.com>
2010-11-27 16:00:21 +0900

    add unittests of make-identity-matrix3 and make-identity-matrix4

--- tests/identity-matrix.lisp
+++ tests/identity-matrix.lisp
@@ -7,7 +7,7 @@
 (require :nurarihyon)
 (require :lisp-unit)
 
-(lisp-unit:define-test make-identity-test
+(lisp-unit:define-test make-identity-matrix-test
   (let* ((dim 10)
          (mat (nurarihyon:make-identity-matrix dim)))
     (dotimes (j dim)
@@ -16,5 +16,21 @@
             (lisp-unit:assert-float-equal (aref mat i j) 1.0d0)
             (lisp-unit:assert-float-equal (aref mat i j) 0.0d0))))
     ))
+
+(lisp-unit:define-test make-identity-matrix3-test
+  (let ((mat (nh:make-identity-matrix3)))
+    (dotimes (i 3)
+      (dotimes (j 3)
+        (if (= i j)
+            (lisp-unit:assert-float-equal (aref mat i j) 1.0d0)
+            (lisp-unit:assert-float-equal (aref mat i j) 0.0d0))))))
+
+(lisp-unit:define-test make-identity-matrix4-test
+  (let ((mat (nh:make-identity-matrix4)))
+    (dotimes (i 4)
+      (dotimes (j 4)
+        (if (= i j)
+            (lisp-unit:assert-float-equal (aref mat i j) 1.0d0)
+            (lisp-unit:assert-float-equal (aref mat i j) 0.0d0))))))
 
 (lisp-unit:run-tests make-identity-test)
