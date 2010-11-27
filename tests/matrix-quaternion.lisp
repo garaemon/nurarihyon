096725d6152ac52a2c8dc6b8ad22c9a3dc1c5edd
garaemon <garaemon@gmail.com>
2010-11-27 14:20:20 +0900

    introduce new macro: define-matrix33->quaternion-test

--- tests/matrix-quaternion.lisp
+++ tests/matrix-quaternion.lisp
@@ -13,7 +13,7 @@
   (let ((m (nh:make-identity-matrix3)))
     (let ((q (nh:matrix33->quaternion m)))
       (lisp-unit:assert-true (nh:eps-vector= q (nh:identity-quaternion)))))
-  ;; 3rd argument
+  ;; 2nd argument
   (let ((m (nh:make-identity-matrix3))
         (buf (nh:make-vector4)))
     (let ((q (nh:matrix33->quaternion m buf)))
@@ -21,28 +21,29 @@
       (lisp-unit:assert-eq q buf)))
   )
 
-(lisp-unit:define-test matrix-quaternion-conversion1
-  (let ((m #d((0.990033 -0.089418 0.108805)
-              (0.099335 0.991028 -0.089418)
-              (-0.099833 0.099335 0.990033)))
-        (q-result #d(0.99638 0.04736 0.052349 0.04736)))
-    (let ((q-compute (nh:matrix33->quaternion m)))
-      (lisp-unit:assert-true (nh:eps-vector= q-result q-compute)))))
+(defmacro define-matrix33->quaternion-test (name matrix quaternion)
+  `(lisp-unit:define-test ,name
+     (let ((m ,matrix)
+           (q-result ,quaternion))
+       (let ((q-computed (nh:matrix33->quaternion m)))
+         (lisp-unit:assert-true (nh:eps-vector= q-result q-computed))))))
 
-(lisp-unit:define-test matrix-quaternion-conversion2
-  (let ((m #d((0.97517 -0.153792 0.159345)
-              (0.097843 0.944703 0.312992)
-              (-0.198669 -0.289629 0.936293)))
-        (q-result #d(0.981856 -0.153439 0.091158 0.064071)))
-    (let ((q-compute (nh:matrix33->quaternion m)))
-      (lisp-unit:assert-true (nh:eps-vector= q-result q-compute)))))
+(define-matrix33->quaternion-test matrix-quaternion-conversion1
+    #d((0.990033 -0.089418 0.108805)
+       (0.099335 0.991028 -0.089418)
+       (-0.099833 0.099335 0.990033))
+    #d(0.99638 0.04736 0.052349 0.04736))
 
-(lisp-unit:define-test matrix-quaternion-conversion3
-  (let ((m #d((0.808307 0.072066 -0.584334)
-              (-0.341747 0.865602 -0.365982)
-              (0.479426 0.49552 0.7243)))
-        (q-result #d(0.921711 0.233669 -0.288528 -0.11224)))
-    (let ((q-compute (nh:matrix33->quaternion m)))
-      (lisp-unit:assert-true (nh:eps-vector= q-result q-compute)))))
+(define-matrix33->quaternion-test matrix-quaternion-conversion2
+    #d((0.97517 -0.153792 0.159345)
+       (0.097843 0.944703 0.312992)
+       (-0.198669 -0.289629 0.936293))
+    #d(0.981856 -0.153439 0.091158 0.064071))
+
+(define-matrix33->quaternion-test matrix-quaternion-conversion3
+    #d((0.808307 0.072066 -0.584334)
+       (-0.341747 0.865602 -0.365982)
+       (0.479426 0.49552 0.7243))
+    #d(0.921711 0.233669 -0.288528 -0.11224))
 
 (nh:disable-nurarihyon-reader-syntax)
